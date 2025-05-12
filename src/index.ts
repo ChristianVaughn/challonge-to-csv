a2c7f157928855543a6e6cde90e8ee16b68ba3e6
Christian Vaughn <me@christianvaughn.net>
2025-05-11 21:43:40 -0700

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 import { Elysia } from "elysia";
 import { SimpleBracketRoutes } from "./routes/simpleBrackets";
+import { cors } from "@elysiajs/cors";
+
 const PORT = process.env.PORT || 3000;
+
 const app = new Elysia()
+.use(
+  cors({
+    origin: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "PUT", "POST", "DELETE"],
+    credentials: true,
+  }),
+)
   .use(SimpleBracketRoutes)
   .get("/", () => "Hello Challonge to CSV")
   .get("/success", () => "Authentication successful! You can now use the CSV export features.")
